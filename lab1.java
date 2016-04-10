b6ed4117f0f7095742be2980074f80939f14958b
krabbe228 <pavlushka95@gmail.com>
2016-04-10 19:53:42 +0300

    Update lab1.java

--- lab1.java
+++ lab1.java
@@ -5,18 +5,14 @@ import java.util.Scanner;
 public class lab1 {
 
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        double[] x = new double[2];
-        double[] y = new double[2];
-        for (int i = 0; i < 2; i++) {
-            System.out.print("Введите значение х" + (i + 1) + ": ");
-            x[i] = control(scn.next());
-            System.out.print("Введите значение y" + (i + 1) + ": ");
-            y[i] = control(scn.next());
-        }
-        System.out.println("Расстояние между точками = " + Math.sqrt(Math.pow((x[1] - x[0]), 2) + Math.pow((y[1] - y[0]), 2)));
+        double d;
+        System.out.println("Введите длину ребра куба: ");
+        d = control(scn.next());
+        System.out.println("Объём куба = " + Math.pow(d, 3) + "\nПлощадь боковой поверхности куба = " + (4 * Math.pow(d, 2)));
     }
+
     private static double control(String x) {
         Scanner scn = new Scanner(System.in);
         boolean b;
@@ -30,6 +26,11 @@ public class lab1 {
                 x = scn.next();
             }
         }while (b==true);
+        while (Double.parseDouble(x) <= 0) {
+            System.out.print("Ошибка ввода. Введите значение заново: ");
+            x = scn.next();
+        }
         return Double.parseDouble(x);
+
     }
 }
